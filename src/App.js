72a6ffbdbda831b3016f905d0f5555314779b933
minha62 <71062967+minha62@users.noreply.github.com>
2021-07-02 02:14:18 +0900

    #2.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Movie({ fav }) { //props.fav == {fav}
+  return <h3>I like {fav}</h3>;
+}
+
+const movieILike = [
+  {}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      <Movie fav="marvel" />
+      <Movie fav="disney" />
+      <Movie fav="ateez" />
     </div>
   );
 }
